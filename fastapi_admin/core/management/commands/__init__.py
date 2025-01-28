fb944a836fe7bab38d8c3be5f2d81330c6ff3232
ahMAD ASSadi <ds1380ahmad@gmail.com>
2025-01-28 17:41:27 +0330

    Move the cli_util functions to the management init file

---
+++ fastapi_admin/core/management/commands/__init__.py
@@ -0,0 +1,41 @@
+from typing import Optional
+from pathlib import Path
+import typer
+
+app=typer.Typer()
+
+
+def validate_name(name: str, entity: str = "app") -> bool:
+    """Validate the name to ensure it follows naming conventions."""
+    if not name.isidentifier():
+        typer.echo(f"Error: '{name}' is not a valid {
+                   entity} name. It must be a valid Python identifier.")
+        return False
+    return True
+
+
+def render_template(
+    env,
+    template_file: str,
+    target_dir: Path,
+    **context,
+) -> None:
+    """Render a template file and save it to the target directory."""
+    template = env.get_template(template_file)
+    rendered_content = template.render(**context)
+
+    # Replace placeholders in the filename and handle template extensions
+    target_file = template_file
+    for key, value in context.items():
+        target_file = target_file.replace(key, value)
+    if target_file.endswith("-tpl"):
+        target_file = target_file[:-4]  # Remove the "-tpl" extension
+
+    target_path = target_dir / target_file
+
+    # Create subdirectories as needed
+    target_path.parent.mkdir(parents=True, exist_ok=True)
+
+    # Write the rendered template to the target file
+    with open(target_path, "w") as f:
+        f.write(rendered_content)
