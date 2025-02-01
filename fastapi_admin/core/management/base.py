f4e9b3123b8c9ab536d32ef0a5d1bb60df004e7b
ahMAD ASSadi <ds1380ahmad@gmail.com>
2025-02-01 22:15:45 +0330

    🔨 Update the management base script and commands initial script

--- fastapi_admin/core/management/base.py
+++ fastapi_admin/core/management/base.py
@@ -1,20 +1,18 @@
+import importlib.resources
+from pathlib import Path
 from typing import List
-import argparse
 
+import fastapi_admin
 
 class BaseCommand:
     help: str = ""
+    template_dir: Path = importlib.resources.files(
+        fastapi_admin).joinpath('templates')
 
     def handle(self, args: List[str]):
         """
         This method must be implemented by all subclasses.
         It contains the logic to execute the command.
         """
-        raise NotImplementedError("Subclasses must implement the 'handle' method.")
-
-    def add_arguments(self, parser: argparse.ArgumentParser):
-        """
-        This method can be implemented by subclasses to add custom arguments to the command.
-        """
-        pass
-
+        raise NotImplementedError(
+            "Subclasses must implement the 'handle' method.")
